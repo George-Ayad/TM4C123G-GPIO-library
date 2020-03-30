d9eca60f3f5779b7d9aa060a2ed2747815809bac
George Gabra Jr <george.gabra.jr@outlook.com>
2020-03-30 23:29:29 +0200

    Pinmode Update
    wrote pinmode function and sample pin definitions and "set pin", "clr pin" and "get pin" definitions

--- gpio.c
+++ gpio.c
@@ -1,1 +1,44 @@
 #include "gpio.h" 
+
+
+void pinmode(int pin, byte dir){
+  // GET PORT
+  char port = int(pin/10000);
+  if (port>3) port =- 16;
+  
+  // GET PIN
+  char pin = int(pin%10);
+  
+  // ENABLE CLK FOR PORT
+  SET_BIT(reg, pin);
+  
+  // UNLOCK PIN
+  (GPIO_PORTA_LOCK_R + int(pin/10)) = 0x4C4F434B;
+  
+  // ENABLE PIN AS DIGITAL
+  reg = (GPIO_PORTA_DEN_R + int(pin/10));
+  SET_BIT(reg, pin);
+  
+  // GET DATA ADDRESS
+  reg = (GPIO_PORTA_DEN_R + int(pin/10));
+  
+  switch(dir){
+    case 0:
+      // SET DIRECTION TO OUTPUT
+      SET_BIT(reg, pin);
+      break;
+      
+    case 1:
+      // SET DIRECTION TO INPUT
+      CLR_BIT(reg, pin);
+      break
+    
+    case 2:
+      // SET DIRECTION TO INPUT WITH PULLUP
+      CLR_BIT(reg, pin);
+      reg = (GPIO_PORTA_PUR_R + int(pin/10));
+      CLR_BIT(reg, pin);
+      break;
+    }
+  }
+}
--- gpio.h
+++ gpio.h
@@ -1,2 +1,17 @@
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
+
+#define PA1     01
+#define PB3     10003
+#define PC5     20005
+#define PF2     210002
+
+#define INPUT   1
+#define OUTPUT  0
+#define PULLUP  2
+
+#define SET_BIT(reg,bitN)       ((reg) |= (1<<bitN))
+#define CLR_BIT(reg,bitN)       ((reg) &= ~(1<<bitN))
+#define GET_BIT(reg,bitN)       (1 & ((reg)>>bitN))
+
+void pinmode(int pin, byte dir);
