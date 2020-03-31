2f05e38a069a517017199b7f4d120807a8c45468
George Gabra Jr <george.gabra.jr@outlook.com>
2020-03-31 22:15:59 +0200

    Fixed runtime errors

--- gpio.c
+++ gpio.c
@@ -1,46 +1,39 @@
 #include "gpio.h" 
 
 
-void pinmode(int pin, char dir){
-  uint32_t reg = 0;
+void pinmode(uint8_t inPortPin, char dir){  
+  // GET OFFSET AND PIN
+  uint16_t offset = 1000*(uint8_t)(inPortPin/10);
+  uint8_t pin = (uint8_t)(inPortPin%10);
   
   // GET PORT
-  uint8_t port = (int)pin/10000;
+  uint8_t port = (uint8_t)inPortPin/10000;
   if (port>3) port -= 16;
-  
-  // GET PIN
-  uint8_t pinbit = (int)pin%10;
   
   // ENABLE CLK FOR PORT
   SET_BIT(SYSCTL_RCGCGPIO_R, port);
   
   // UNLOCK PIN
-  reg = (GPIO_PORTA_LOCK_R + (int)pin/10);
-  reg = 0x4C4F434B;
+  (*((volatile uint32_t *)(0x40004520+offset))) = 0x4C4F434B; //DIRECTION
   
   // ENABLE PIN AS DIGITAL
-  reg = (GPIO_PORTA_DEN_R + (int)pin/10);
-  SET_BIT(reg, pinbit);
-  
-  // GET DIRECTION ADDRESS
-  reg = (GPIO_PORTA_DIR_R + (int)pin/10);
-  
+  SET_BIT( (*((volatile uint32_t *)(0x4000451C+offset))) , pin); //DIGITAL ENABLE
+
   switch(dir){
     case 0:
       // SET DIRECTION TO OUTPUT
-      SET_BIT(reg, pinbit);
+      SET_BIT( (*((volatile uint32_t *)(0x40004400+offset))) , pin); //DIRECTION
       break;
       
     case 1:
       // SET DIRECTION TO INPUT
-      CLR_BIT(reg, pinbit);
+      CLR_BIT( (*((volatile uint32_t *)(0x40004400+offset))) , pin); //DIRECTION
       break;
     
     case 2:
-      // SET DIRECTION TO INPUT WITH PULLUP
-      CLR_BIT(reg, pinbit);
-      reg = (GPIO_PORTA_PUR_R + (int)pin/10);
-      CLR_BIT(reg, pinbit);
+      // SET DIRECTION TO INPUT WITH AND SET PULLUP
+      CLR_BIT( (*((volatile uint32_t *)(0x40004400+offset))) , pin); //DIRECTION
+      CLR_BIT( (*((volatile uint32_t *)(0x40004510+offset))) , pin); //PULLUP
       break;
   }
 }
--- gpio.h
+++ gpio.h
@@ -2,16 +2,21 @@
 #include "tm4c123gh6pm.h"
 
 #define PA1     01
-#define PB3     10003
-#define PC5     20005
-#define PF2     210002
+#define PB3     13
+#define PC5     25
+#define PD3     33
+#define PE1     201
+#define PF2     212
 
 #define INPUT   1
 #define OUTPUT  0
 #define PULLUP  2
 
+#define HIGH    1
+#define LOW     0
+
 #define SET_BIT(reg,bitN)       ((reg) |= (1<<bitN))
 #define CLR_BIT(reg,bitN)       ((reg) &= ~(1<<bitN))
 #define GET_BIT(reg,bitN)       (1 & ((reg)>>bitN))
 
-void pinmode(int pin, char dir);
+void pinmode(uint8_t pin, char dir);
