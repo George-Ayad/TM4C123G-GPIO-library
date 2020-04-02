41fa63ee4b49363a43aa8de205cd7c813d0905e8
George Gabra Jr <george.gabra.jr@outlook.com>
2020-04-02 18:51:22 +0200

    Fixed missing initialization statments

--- main.c
+++ main.c
@@ -9,16 +9,19 @@ void delay_ms(int nCount){ /* Wait function */
 int main()
 {
   pinmode(RLED, OUTPUT);
+  pinmode(GLED, OUTPUT);
+  pinmode(BLED, OUTPUT);
   pinmode(PushButton2, PULLUP);
+  pinmode(PushButton1, PULLUP);
   
   while (1){
     pinwrite(RLED, HIGH);
     delay_ms(100);
     pinwrite(RLED, LOW);
     delay_ms(100);
-    if(pinread(PushButton1) == HIGH)pinwrite(BLED, HIGH);
+    if(pinread(PushButton1) == LOW)pinwrite(BLED, HIGH);
     else pinwrite(BLED, LOW);
-    if(pinread(PushButton2) == HIGH)pinwrite(GLED, HIGH);
+    if(pinread(PushButton2) == LOW)pinwrite(GLED, HIGH);
     else pinwrite(GLED, LOW);
   }
   return 0;
